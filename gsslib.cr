058e6a2c04987d6d4b2ee7a88c6df45c2965df89
Brady Wied <brady@bswtechconsulting.com>
2017-12-08 01:59:55 -0700

    DRY/make this more understandable

--- gsslib.cr
+++ gsslib.cr
@@ -16,6 +16,8 @@ lib GssLib
   end
 
   type NameStruct = Void*
+  alias MinorStatusPtr = UInt32*
+  alias MajorStatus = UInt32
   type CredentialStruct = Void*
   alias GssCredentialUsageFlags = Int32
 
@@ -25,21 +27,21 @@ lib GssLib
   # $gss_c_nt_user_name = __gss_c_nt_user_name_oid_desc : Oid
 
   # output_name is a pointer of a pointer
-  fun gss_import_name(minor_status_ptr : UInt32*,
+  fun gss_import_name(minor_status_ptr : MinorStatusPtr,
                       buffer : Buffer*,
                       oid : Oid*,
-                      output_name : NameStruct*) : UInt32
+                      output_name : NameStruct*) : MajorStatus
 
-  fun gss_release_buffer(minor_status_ptr : UInt32*,
-                         buffer : Buffer*) : UInt32
+  fun gss_release_buffer(minor_status_ptr : MinorStatusPtr,
+                         buffer : Buffer*) : MajorStatus
 
-  fun gss_release_name(minor_status_ptr : UInt32*,
-                       name : NameStruct*) : UInt32
+  fun gss_release_name(minor_status_ptr : MinorStatusPtr,
+                       name : NameStruct*) : MajorStatus
 
-  fun gss_release_cred(minor_status_ptr : UInt32*,
-                       cred : CredentialStruct*) : UInt32
+  fun gss_release_cred(minor_status_ptr : MinorStatusPtr,
+                       cred : CredentialStruct*) : MajorStatus
 
-  fun gss_acquire_cred_with_password(minor_status_ptr : UInt32*,
+  fun gss_acquire_cred_with_password(minor_status_ptr : MinorStatusPtr,
                                      name : NameStruct,
                                      password: Buffer*,
                                      time: Int32,
@@ -47,14 +49,14 @@ lib GssLib
                                      cred_usage: GssCredentialUsageFlags,
                                      credential_id: CredentialStruct*,
                                      actual_mechs: OidSet**,
-                                     time_rec: Int32*) : UInt32
+                                     time_rec: Int32*) : MajorStatus
 
-  fun gss_display_status(minor_status_ptr : UInt32*,
+  fun gss_display_status(minor_status_ptr : MinorStatusPtr,
                          status_code : UInt32,
                          status_type : Int32,
                          mechanism_type: Oid*,
                          message_context: UInt32*,
-                         buffer : Buffer*) : UInt32
+                         buffer : Buffer*) : MajorStatus
 end
 
 @[Link("bwwrapper")]
