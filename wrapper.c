1954d26c24c10d1a4a3ea7a3ea21c6b69efd75a4
Brady Wied <brady@bswtechconsulting.com>
2017-12-07 23:11:59 -0700

    Verified pointer address is the same

--- wrapper.c
+++ wrapper.c
@@ -1,5 +1,7 @@
 #include <gssapi/gssapi.h>
+#include <stdio.h>
 
 gss_OID bsw_gss_nt_user_name() {
+  printf("Wrapper; returning GSS_C_NT_USER_NAME at memory address %p\n",GSS_C_NT_USER_NAME);
   return GSS_C_NT_USER_NAME;
 }
