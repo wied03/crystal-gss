3ca825908dc9e15d5e83479c2b5c38bf0728d388
Brady Wied <brady@bswtechconsulting.com>
2017-12-07 23:40:02 -0700

    Use begin/ensure syntax

--- Main.cr
+++ Main.cr
@@ -7,10 +7,15 @@ buffer.length = spn.size + 1
 name_result = uninitialized KrbWrapper::NameStruct
 minor_status = uninitialized Int32
 status = KrbWrapper.gss_import_name(pointerof(minor_status),
-  pointerof(buffer),
-  BswWrapper.bsw_gss_nt_user_name,
-  pointerof(name_result))
+                                    pointerof(buffer),
+                                    BswWrapper.bsw_gss_nt_user_name,
+                                    pointerof(name_result))
 
 raise "Problem!" unless status == 0
-puts "Name created, now disposing name"
-KrbWrapper.gss_release_name(pointerof(minor_status), pointerof(name_result))
+begin
+  puts "Name created"
+ensure
+  puts "Releasing name..."
+  KrbWrapper.gss_release_name(pointerof(minor_status),
+                              pointerof(name_result))
+end
