779474600428d3dfccb50219748e4aa00e22f45c
Brady Wied <brady@bswtechconsulting.com>
2017-12-09 21:47:07 -0700

    try and re-enable this

--- gssapi/gss_extern_variable_fetcher.c
+++ gssapi/gss_extern_variable_fetcher.c
@@ -1,6 +1,7 @@
 #include <gssapi/gssapi.h>
 
 // for why this C code is in here, see comments in binding.cr re: https://github.com/crystal-lang/crystal/issues/4845
+extern gss_OID gss_mech_krb5;
 
 gss_OID gss_nt_user_name() {
   return GSS_C_NT_USER_NAME;
@@ -10,9 +11,9 @@ gss_OID gss_host_based_service() {
   return GSS_C_NT_HOSTBASED_SERVICE;
 }
 
-// gss_OID gss_krb5_mechanism() {
-//   return GSS_KRB5_MECHANISM;
-// }
+gss_OID gss_krb5_mechanism() {
+  return gss_mech_krb5;
+}
 
 // gss_OID gss_spnego_mechanism() {
 //   return GSS_SPNEGO_MECHANISM;
