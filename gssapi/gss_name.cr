eca9d81c72e7e1af1de6393e3a2ec12d9c2361d1
Brady Wied <brady@bswtechconsulting.com>
2017-12-09 21:17:27 -0700

    More accurate name

--- gssapi/gss_name.cr
+++ gssapi/gss_name.cr
@@ -3,12 +3,12 @@ module GssApi
     @structure : GssApi::GssLib::NameStruct
     getter structure
 
-    def initialize(upn : String,
+    def initialize(principal : String,
                    oid : GssApi::GssMechanism)
       @closed = false
       buffer = GssApi::GssLib::Buffer.new
-      buffer.value = upn
-      buffer.length = upn.size
+      buffer.value = principal
+      buffer.length = principal.size
       invoker = GssApi::FunctionInvoker(GssApi::GssLib::NameStruct).new("gss_import_name")
       @structure = invoker.invoke do |minor_pointer|
         status = GssApi::GssLib.gss_import_name(minor_pointer,
