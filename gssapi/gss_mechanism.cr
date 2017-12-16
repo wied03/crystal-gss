3406d4604b83b087ba94b20dbb2151a97a47736e
Brady Wied <brady@bswtechconsulting.com>
2017-12-15 21:48:01 -0700

    Formatting

--- gssapi/gss_mechanism.cr
+++ gssapi/gss_mechanism.cr
@@ -1,8 +1,8 @@
 module GssApi
   class GssMechanism
-    NT_USER_NAME = GssMechanism.new("NT_USER_NAME", GssApi::GssLib.gss_nt_user_name)
+    NT_USER_NAME          = GssMechanism.new("NT_USER_NAME", GssApi::GssLib.gss_nt_user_name)
     NT_HOST_BASED_SERVICE = GssMechanism.new("NT_HOST_BASED_SERVICE", GssApi::GssLib.gss_host_based_service)
-    SPNEGO = GssMechanism.new("SPNEGO", GssApi::GssExternVariableFetcher.gss_spnego_mechanism)
+    SPNEGO                = GssMechanism.new("SPNEGO", GssApi::GssExternVariableFetcher.gss_spnego_mechanism)
 
     getter underlying
 
